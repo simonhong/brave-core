80e54a82ee5f4ce8652e5585f03f605cc33b56dc
Mark Pilgrim <mpilgrim@brave.com>
2020-05-13 12:31:40 -0400

    Implement canvas blocking v2

--- chromium_src/third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ chromium_src/third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -3,8 +3,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "brave/renderer/brave_content_settings_agent_impl_helper.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+
+#define BRAVE_IS_POINT_IN_PATH                                 \
+  LocalDOMWindow* window = LocalDOMWindow::From(script_state); \
+  if (window && !AllowFingerprinting(window->GetFrame())) {    \
+    return false;                                              \
+  }
+
+#define BRAVE_IS_POINT_IN_STROKE BRAVE_IS_POINT_IN_PATH
 
 #define BRAVE_GET_IMAGE_DATA                                         \
   LocalDOMWindow* window = LocalDOMWindow::From(script_state);       \
